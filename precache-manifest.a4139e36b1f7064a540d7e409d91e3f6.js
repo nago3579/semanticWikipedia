e6fdf83cbf5d78a49d911a1033dc95d8f3829c2b
nago3579 <nago3579@colorado.edu>
2020-01-30 15:32:17 -0700

    Updates

---
+++ precache-manifest.a4139e36b1f7064a540d7e409d91e3f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f68cef8162257f18a45a1b391f2e30",
+    "url": "/semanticWikipedia/index.html"
+  },
+  {
+    "revision": "858e8cace6afb8d5508a",
+    "url": "/semanticWikipedia/static/css/main.82b07595.chunk.css"
+  },
+  {
+    "revision": "c09a515d3018a04306e4",
+    "url": "/semanticWikipedia/static/js/2.c07e5bbd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/semanticWikipedia/static/js/2.c07e5bbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "858e8cace6afb8d5508a",
+    "url": "/semanticWikipedia/static/js/main.3009fbc4.chunk.js"
+  },
+  {
+    "revision": "867f663bcc2ff983e85a",
+    "url": "/semanticWikipedia/static/js/runtime-main.f2061d03.js"
+  },
+  {
+    "revision": "db968273fc8fecfd3e0796cc3d8c9089",
+    "url": "/semanticWikipedia/static/media/wikipedia-logo.db968273.jpg"
+  }
+]);
